457f761c60c4c629b8ef8c76bfdc6073349ebf5a
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-09-17 14:27:08 +0300

    style(react-version):add base style to Textarea component

--- react-version/src/Textarea/Textarea.jsx
+++ react-version/src/Textarea/Textarea.jsx
@@ -1,12 +1,15 @@
 export default function Textarea() {
   return (
-    <article>
-      <header>
-        <h1>Write your Message</h1>
+    <article className=" w-[40%] mx-auto mt-[50px] ">
+      <header className="w-[100%] mb-[1rem]">
+        <h1 className="text-2xl font-medium">Write your Message</h1>
       </header>
-      <section>
-        <textarea placeholder="Start typing..." maxLength={250}></textarea>
-        <p>0 / 250</p>
+      <section className="relative">
+        <textarea
+          className="w-full h-[150px] p-[1rem] rounded-2xl resize-none border-2"
+          placeholder="Start typing..."
+          maxLength={250}></textarea>
+        <p className="absolute right-[1rem] bottom-[1rem]">0 / 250</p>
       </section>
     </article>
   );
